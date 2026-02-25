f2a807ad3aafcbbb2e131cc239e6b1583da88838
MURUGAVEL V <vmv2k05@gmail.com>
2026-02-25 13:32:01 +0530

    Remove favicon extraction scripts and products table migration files

--- extract-favicon.cjs
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp');
-
-sharp('public/ponsri-logo.png')
-    .extract({ left: 0, top: 0, width: 222, height: 222 })
-    .resize(48, 48)
-    .toFile('public/favicon.png')
-    .then(() => console.log('Favicon extracted successfully'))
-    .catch(err => console.error('Error extracting favicon:', err));
